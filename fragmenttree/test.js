de61ce3aa166d3288b1447da97f1d701c305cc8b
camhux <camzig@gmail.com>
2015-09-21 17:11:56 -0700

    Write more tests to fail

--- fragmenttree/test.js
+++ fragmenttree/test.js
@@ -8,20 +8,66 @@ function FragmentMock(words) {
 
 describe("Fragment tree", function() {
 
-  var fragments = [];
+  describe("Creation", function() {
 
-  fragments.push(new FragmentMock(["hi", "what's", "up", "kid"]));
-  fragments.push(new FragmentMock(["hi", "what's", "up", "friend"]));
-  fragments.push(new FragmentMock(["hi", "what's", "going", "on"]));
+    var fragments = [];
 
-  it("should accept an array of fragments as an argument", function() {
+    fragments.push(new FragmentMock(["hi", "what's", "up", "kid"]));
+    fragments.push(new FragmentMock(["hi", "what's", "up", "friend"]));
+    fragments.push(new FragmentMock(["hi", "what's", "going", "on"]));
+
+    it("should accept an array of fragments as an argument", function() {
+      var tree = new FragmentTree(fragments);
+      expect(tree).to.be.ok;
+    });
+
+    it("should return the root node of the tree", function() {
+      var tree = new FragmentTree(fragments);
+      expect(tree).to.be.an("object");
+    });
+
+    it("should have a traversable depth of 4", function() {
+      var tree = new FragmentTree(fragments);
+      var depth = 0;
+      var node = tree.root;
+
+      while (Object.keys(node.children).length !== 0) {
+        node = node.children[Object.keys(node.children)[0]];
+        depth += 1;
+      }
+
+      expect(depth).to.equal(4);
+
+    });
+
+  }); // Creation
+
+  describe("Retrieval", function() {
+    var fragments = [];
+
+    fragments.push(new FragmentMock(["can", "barely", "handle", "it"]));
+    fragments.push(new FragmentMock(["can", "barely", "deal", "with"]));
+    fragments.push(new FragmentMock(["can", "only", "guess", "what"]));
+    fragments.push(new FragmentMock(["who", "cought", "the", "wom"]));
+    fragments.push(new FragmentMock(["who", "can", "believe", "it"]));
+
     var tree = new FragmentTree(fragments);
-    expect(tree).to.be.ok;
-  });
 
-  it("should return the root node of the tree", function() {
-    var tree = new FragmentTree(fragments);
-    expect(tree).to.be.an("object");
-  });
+    it("should be able to dereference to a fragment from any child node", function() {
+      var child = tree.root.children["can"].children["barely"].children["deal"];
+      expect(tree.fragHash[child.fragmentRefs[0]]).to.be.an.instanceOf(FragmentMock);
+    });
 
-});
+    it("should have a method called retrieve", function() {
+      expect(tree.retrieve).to.be.a("function");
+    });
+
+    it("should use the argument to retrieve to find an overlapping fragment", function() {
+      var backsubstr = ["he", "can", "barely", "deal"];
+      var match = tree.retrieve(backsubstr);
+      expect(match).to.be.an.instanceOf(FragmentMock);
+    });
+
+  }); // Retrieval
+
+}); // FragmentTree
